55de0b1472160ca6a76d9d65aaf51e508314bac5
alirezafarin <alirafarinzad@gmail.com>
2023-11-12 16:34:54 +0330

    config: changed some configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      colors: {
-        primary: '#5b9a3f',
-      },
+      colors: {},
     },
   },
   plugins: [],
