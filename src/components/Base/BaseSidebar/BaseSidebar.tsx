4faf942cb6cce0f4357d3ca8c3e7e1cd88e862eb
alirezafarin <alirafarinzad@gmail.com>
2023-11-13 18:16:47 +0330

    style: make sidebar compoonent responsive

--- src/components/Base/BaseSidebar/BaseSidebar.tsx
+++ src/components/Base/BaseSidebar/BaseSidebar.tsx
@@ -12,7 +12,7 @@ export const BaseSidebar: FC<IBaseSidebarProps> = ({ children, open, className =
   return (
     <div
       className={clsx(
-        'fixed top-0 right-0 z-40 w-3/5 h-screen p-4 overflow-y-auto transition-transform bg-white',
+        'fixed top-0 right-0 z-40 md:w-3/5 w-screen h-screen p-4 overflow-y-auto transition-transform bg-white',
         styles.sidebar,
         { 'translate-x-full': !open },
         className,
